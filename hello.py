6ab78708e99560f9c7a9d73911a14ceeb477b0bb
leewang <lirenxn@gmail.com>
2020-05-04 19:59:01 +1000

    Try template 5    - install flask-moment    - bind flask-moment to app    - use moment in template

--- hello.py
+++ hello.py
@@ -2,14 +2,17 @@ from flask import Flask, request, g, session, abort, redirect, render_template
 from flask.helpers import make_response
 
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+mmt = Moment(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/usr/<name>')
